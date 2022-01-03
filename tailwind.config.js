5510cccab06432c59ce56eceb33cc3f550098dcc
Ris Adams <chris@tuesdaybegins.com>
2022-01-03 13:59:29 -0500

    Update to TW3, Rebuild defaults

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+};
